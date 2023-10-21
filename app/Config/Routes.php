09b221f4880c71a8d6d0d68356f6efeeba94d9ee
Khadafi Rohman Prihanda <dafi.pri19@gmail.com>
2023-10-21 12:04:14 +0700

    solving cors authorization

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,6 +13,6 @@ $routes->post('/masuk', [Pengguna::class, 'masuk']);
 $routes->post('/profil-pengguna', [Pengguna::class, 'getData']);
 
 $routes->get('/semua-berita', [Berita::class, 'show'], ['filter' => 'authFilter']);
-$routes->post('/tambah-berita', [Berita::class, 'create']);
+$routes->post('/tambah-berita', [Berita::class, 'create'], ['filter' => 'authFilter']);
 $routes->post('/edit-berita', [Berita::class, 'edit'], ['filter' => 'authFilter']);
 $routes->delete('/hapus-berita', [Berita::class, 'delete'], ['filter' => 'authFilter']);
